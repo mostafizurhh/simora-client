5af284b572d2ca154675a004bba167f460688214
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-11-12 05:31:16 +0000

    bg modified

--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import chair from '../../assets/images/chair.jpeg'
-
+import chair from '../../assets/images/chair.jpeg';
+import './Banner.css'
 
 
 const Banner = () => {
     return (
-        <div className="hero" >
-            <div className="hero-content flex-col lg:flex-row-reverse bg-[url('../../assets/images/bg.png')]">
+        <div className="hero banner w-full h-full bg-no-repeat bg-cover" style={{ backgroundImage: '../../assets/images/chair.jpeg' }} >
+            <div className="hero-content flex-col lg:flex-row-reverse">
                 <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt='' />
                 <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                    <h1 className="text-5xl font-bold text-white">Box Office News!</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-
                     <button className="btn btn-primary bg-gradient-to-r from-teal-300 to-teal-400 text-white">Get Started</button>
                 </div>
             </div>
