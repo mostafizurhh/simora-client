58737f6110e3966b75c120ae87ee84e2890e0786
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-11-18 02:22:44 +0000

    update Admin role for a user by creating an API and verifyJWT, used refetch in handleMakeAdmin function to load data instantly.

--- src/pages/DashBoard/AllUsers.js
+++ src/pages/DashBoard/AllUsers.js
@@ -1,10 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import toast from 'react-hot-toast';
+import Spinner from '../Shared/Spinner/Spinner';
 
 const AllUsers = () => {
 
+
     /* load all users data from server */
-    const { data: users = [] } = useQuery({
+    const { data: users = [], isLoading, refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await fetch('http://localhost:5000/users');
@@ -13,6 +16,22 @@ const AllUsers = () => {
         }
     })
 
+    const handleMakeAdmin = id => {
+        fetch(`http://localhost:5000/users/admin/${id}`, {
+            method: 'PUT'
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    toast.success('You are an Admin now!!', { duration: 4000 });
+                    refetch();
+                }
+            })
+    }
+
+    if (isLoading) {
+        return <Spinner></Spinner>
+    }
 
     return (
         <div>
@@ -24,6 +43,8 @@ const AllUsers = () => {
                             <th></th>
                             <th>Name</th>
                             <th>email</th>
+                            <th>Role</th>
+                            <th>Remove Data</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -33,6 +54,8 @@ const AllUsers = () => {
                                     <th>{i + 1}</th>
                                     <td>{user.name}</td>
                                     <td>{user.email}</td>
+                                    <td>{user?.role !== 'admin' && <button onClick={() => handleMakeAdmin(user._id)} className='btn btn-secondary btn-sm'>Make Admin</button>}</td>
+                                    <td><button className='btn btn-error btn-sm'>Delete</button></td>
                                 </tr>
                             )
                         }
