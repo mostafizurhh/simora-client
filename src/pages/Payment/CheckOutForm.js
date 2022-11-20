df7940f09d090317f69d8b03c1a5f9bd4adc0637
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-11-20 17:40:47 +0000

    stored users payment info in DB and set loader while loding data from server

--- src/pages/Payment/CheckOutForm.js
+++ src/pages/Payment/CheckOutForm.js
@@ -1,32 +1,46 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 
-const CheckOutForm = () => {
+const CheckOutForm = ({ booking }) => {
+    const { price, customer, email, _id } = booking;
     const stripe = useStripe();
     const elements = useElements();
     const [error, setError] = useState('')
+    const [success, setSuccess] = useState('')
+    const [loading, setLoading] = useState(false)
+    const [transactionId, setTransactionId] = useState('')
+    const [clientSecret, setClientSecret] = useState("");
+
+
+    useEffect(() => {
+        // Create PaymentIntent as soon as the page loads
+        fetch("http://localhost:5000/create-payment-intent", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                authorization: `bearer ${localStorage.getItem('accessToken')}`
+            },
+            body: JSON.stringify({ price }),
+        })
+            .then((res) => res.json())
+            .then((data) => setClientSecret(data.clientSecret));
+    }, [price]);
 
     const handleSubmit = async (event) => {
         // Block native form submission.
         event.preventDefault();
 
         if (!stripe || !elements) {
-            // Stripe.js has not loaded yet. Make sure to disable
-            // form submission until Stripe.js has loaded.
             return;
         }
 
-        // Get a reference to a mounted CardElement. Elements knows how
-        // to find your CardElement because there can only ever be one of
-        // each type of element.
         const card = elements.getElement(CardElement);
 
         if (card == null) {
             return;
         }
 
-        // Use your card Element with other Stripe.js APIs
         const { error, paymentMethod } = await stripe.createPaymentMethod({
             type: 'card',
             card,
@@ -38,33 +52,93 @@ const CheckOutForm = () => {
         } else {
             setError('');
         }
+
+        setSuccess('');
+        setLoading(true)
+
+        const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: customer,
+                        email: email
+                    },
+                },
+            },
+        );
+
+        if (confirmError) {
+            setError(confirmError.message);
+            return;
+        }
+
+        if (paymentIntent.status === "succeeded") {
+
+            /* save user åayment info in DB */
+            const payment = {
+                name: customer,
+                price,
+                bookingId: _id,
+                email,
+                transactionId: paymentIntent.id
+            }
+
+            fetch("http://localhost:5000/userPayments", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    authorization: `bearer ${localStorage.getItem('accessToken')}`
+                },
+                body: JSON.stringify(payment),
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.insertedId) {
+                        setSuccess('Payment is Successful');
+                        setTransactionId(paymentIntent.id);
+                    }
+
+                })
+        }
+        setLoading(false)
+        console.log('payment-intent', paymentIntent)
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <CardElement
-                options={{
-                    style: {
-                        base: {
-                            fontSize: '16px',
-                            color: '#424770',
-                            '::placeholder': {
-                                color: '#aab7c4',
+        <>
+            <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
                             },
                         },
-                        invalid: {
-                            color: '#9e2146',
-                        },
-                    },
-                }}
-            />
-            <button className='btn btn-sm btn-success mt-8 mb-5' type="submit" disabled={!stripe}>
-                Pay
-            </button>
-            <div>
-                {error}
-            </div>
-        </form>
+                    }}
+                />
+                <button className='btn btn-sm btn-success mt-8 mb-5' type="submit"
+                    disabled={!stripe || !clientSecret || loading}>
+                    Pay
+                </button>
+            </form>
+            <p className='text-red-600 font-normal'> {error}</p>
+            {success &&
+                <div>
+                    <p className='text-green-600 font-bold text-xl'>{success}</p>
+                    <p className='text-lg italic'>Your Transaction Id: <span className='font-bold'>{transactionId}</span></p>
+                </div>
+            }
+        </>
     );
 };
 
--- src/pages/Payment/Payment.js
+++ src/pages/Payment/Payment.js
@@ -1,24 +1,30 @@
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigation } from 'react-router-dom';
+import Spinner from '../Shared/Spinner/Spinner';
 import CheckOutForm from './CheckOutForm';
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK)
 console.log(stripePromise);
 
 const Payment = () => {
-    const data = useLoaderData()
-    console.log(data)
+    const booking = useLoaderData()
+    const navigation = useNavigation();
+
+    if (navigation.state === 'loading') {
+        return <Spinner></Spinner>
+    }
 
     return (
-        <div>
-            <h2 className='text-3xl font-bold mt-8'>Payment for <span className='text-secondary'>{data.treatmentName}</span></h2>
-            <h2 className='text-xl text-orange-600 font-bold mt-3 mb-5'>Price: ${data.price}</h2>
+        <div className='container mx-auto'>
+            <h2 className='text-3xl font-bold mt-8'>Payment for <span className='text-secondary'>{booking.treatmentName}</span></h2>
+            <h2 className='text-xl text-orange-600 font-bold mt-3 mb-5'>Price: ${booking.price}</h2>
             <div className='card w-96 bg-base-200 shadow-xl'>
                 <div className='card-body'>
                     <Elements stripe={stripePromise}>
-                        <CheckOutForm />
+                        <CheckOutForm
+                            booking={booking} />
                     </Elements>
                 </div>
             </div>
